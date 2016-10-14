8517ef5d434311b042ec7c381cf751edd92560bb
mgaulmin <matthieu.gaulmin@gmail.com>
2016-10-14 14:29:08 +0200

    Acquisition activity 10-14-2016

--- FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/MainActivity.java
+++ FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.flightdataacquisition;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user clicks the get started button */
+    public void sendStarted(View view) {
+        Intent intent = new Intent(this, AcquisitionActivity.class);
+        startActivity(intent);
     }
 }
