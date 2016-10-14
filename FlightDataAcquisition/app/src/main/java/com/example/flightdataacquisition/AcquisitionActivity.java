1e772368504b7b8d475b329358527a9afd1291c9
mgaulmin <matthieu.gaulmin@gmail.com>
2016-10-14 17:02:56 +0200

    Buttons interaction 10-14-2016

--- FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/AcquisitionActivity.java
+++ FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/AcquisitionActivity.java
@@ -3,14 +3,27 @@ package com.example.flightdataacquisition;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class AcquisitionActivity extends AppCompatActivity {
+
+    ProgressBar Pbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acquisition);
 
-        Intent intent = getIntent();
+        Pbar = (ProgressBar)findViewById(R.id.progress);
+        Pbar.setVisibility(View.INVISIBLE);
+        Button button_start = (Button)findViewById(R.id.startButton);
+        button_start.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Pbar.setVisibility(View.VISIBLE);
+            }
+        });
     }
 }
