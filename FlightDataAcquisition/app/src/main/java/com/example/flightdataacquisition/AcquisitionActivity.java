9d62cbdcc6ab3f0c77339ffbb399809394387619
mgaulmin <matthieu.gaulmin@gmail.com>
2016-10-17 16:42:58 +0200

    Sensors connectivity 10-17-2016

--- FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/AcquisitionActivity.java
+++ FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/AcquisitionActivity.java
@@ -1,6 +1,11 @@
 package com.example.flightdataacquisition;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -8,15 +13,30 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 
-public class AcquisitionActivity extends AppCompatActivity {
+public class AcquisitionActivity extends AppCompatActivity implements SensorEventListener {
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
 
     ProgressBar Pbar;
     TextView AcqText;
 
     @Override
+    public final void onAccuracyChanged(Sensor sensor, int accuracy) {
+        // Do something here if sensor accuracy changes.
+    }
+
+    public final void onSensorChanged(SensorEvent event) {
+        // The light sensor returns a single value.
+        // Many sensors return 3 values, one for each axis.
+        // Do something with this sensor value.
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acquisition);
+
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
         Pbar = (ProgressBar)findViewById(R.id.progress);
         Pbar.setVisibility(View.INVISIBLE);
