821d58f727c52e2d905076639aef20e970ddea3c
mgaulmin <matthieu.gaulmin@gmail.com>
2016-11-18 13:37:49 +0100

    11-18-2016 Location acquisition running

--- FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/AcquisitionActivity.java
+++ FlightDataAcquisition/app/src/main/java/com/example/flightdataacquisition/AcquisitionActivity.java
@@ -11,28 +11,41 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
 public class AcquisitionActivity extends AppCompatActivity implements GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.OnConnectionFailedListener, LocationListener {
 
     // Define variables
     ProgressBar Pbar;
     TextView AcqText;
 
-    private static final String TAG = "AcquisitionActivity";
-    private TextView mLatitudeTextView;
-    private TextView mLongitudeTextView;
+    private static final String TAG = AcquisitionActivity.class.getSimpleName();
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 1000;
 
+    private Location mLastLocation;
     private GoogleApiClient mGoogleApiClient;
-    private Location mLocation;
 
+    private boolean mRequestLocationUpdates = false;
+
+    private LocationRequest mLocationRequest;
+
+    private static int UPDATE_INTERVAL = 1000;
+    private static int FATEST_INTERVAL = 500;
+    private static int DISPLACEMENT = 10;
+
+    private TextView lblLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acquisition);
+
+        lblLocation = (TextView) findViewById(R.id.longitude_textview);
 
         Pbar = (ProgressBar) findViewById(R.id.progress);
         Pbar.setVisibility(View.INVISIBLE);
@@ -40,6 +53,10 @@ public class AcquisitionActivity extends AppCompatActivity implements GoogleApiC
         AcqText = (TextView) findViewById(R.id.acqText);
         AcqText.setVisibility(View.INVISIBLE);
 
+        if(checkPlayServices()) {
+            buildGoogleApiClient();
+            createLocationRequest();
+        }
 
         Button button_start = (Button) findViewById(R.id.startButton);
         button_start.setOnClickListener(new View.OnClickListener() {
@@ -48,6 +65,9 @@ public class AcquisitionActivity extends AppCompatActivity implements GoogleApiC
             public void onClick(View v) {
                 Pbar.setVisibility(View.VISIBLE);
                 AcqText.setVisibility(View.VISIBLE);
+                displayLocation();
+                mRequestLocationUpdates = true;
+                startLocationUpdates();
 
             }
         });
@@ -59,56 +79,130 @@ public class AcquisitionActivity extends AppCompatActivity implements GoogleApiC
             public void onClick(View v) {
                 Pbar.setVisibility(View.INVISIBLE);
                 AcqText.setVisibility(View.INVISIBLE);
+                mRequestLocationUpdates = false;
+                stopLocationUpdates();
             }
         });
-
-        mLatitudeTextView = (TextView) findViewById((R.id.latitude_textview));
-        mLongitudeTextView = (TextView) findViewById((R.id.longitude_textview));
-
-        mGoogleApiClient = new GoogleApiClient.Builder(this)
-                .addConnectionCallbacks(this)
-                .addOnConnectionFailedListener(this)
-                .addApi(LocationServices.API)
-                .build();
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        mGoogleApiClient.connect();
+        if(mGoogleApiClient != null) {
+            mGoogleApiClient.connect();
+        }
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        checkPlayServices();
+        if(mGoogleApiClient.isConnected() && mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
 
     @Override
     protected void onStop() {
         super.onStop();
-        if (mGoogleApiClient.isConnected()) {
+
+        if(mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
     }
 
     @Override
-    public void onConnected(Bundle bundle) {
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
+    }
+
+    private void displayLocation() {
         try {
-        mLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if (mLocation != null) {
-            mLatitudeTextView.setText(String.valueOf(mLocation.getLatitude()));
-            mLongitudeTextView.setText(String.valueOf(mLocation.getLongitude()));
-        } else {
-            Toast.makeText(this, "Location not Detected", Toast.LENGTH_SHORT).show();
+            mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        } catch (SecurityException e) {
+            e.printStackTrace();
         }
+        if(mLastLocation != null) {
+            double latitude = mLastLocation.getLatitude();
+            double longtitude = mLastLocation.getLongitude();
+
+            lblLocation.setText(latitude + ", " + longtitude);
+        } else {
+            lblLocation.setText(R.string.loc_error);
+        }
+    }
+
+    protected synchronized void buildGoogleApiClient() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API).build();
+    }
+
+    protected void createLocationRequest() {
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(UPDATE_INTERVAL);
+        mLocationRequest.setFastestInterval(FATEST_INTERVAL);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        mLocationRequest.setSmallestDisplacement(DISPLACEMENT);
+    }
+
+    private boolean checkPlayServices() {
+        GoogleApiAvailability googleAPI = GoogleApiAvailability.getInstance();
+        int resultCode = googleAPI.isGooglePlayServicesAvailable(this);
+        if(resultCode != ConnectionResult.SUCCESS) {
+            if(googleAPI.isUserResolvableError(resultCode)) {
+                googleAPI.getErrorDialog(this, resultCode, PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "This device is not supported", Toast.LENGTH_LONG)
+                        .show();
+                finish();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    protected void startLocationUpdates() {
+        try {
+            LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
         } catch (SecurityException e) {
             e.printStackTrace();
         }
     }
 
+    protected void stopLocationUpdates() {
+        LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        displayLocation();
+
+        if(mRequestLocationUpdates) {
+            startLocationUpdates();
+        }
+    }
+
     @Override
     public void onConnectionSuspended(int i) {
-        Log.i(TAG, "Connection Suspended");
         mGoogleApiClient.connect();
     }
 
     @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+
+        Toast.makeText(getApplicationContext(), "Location changed!", Toast.LENGTH_SHORT).show();
+
+        displayLocation();
+    }
+
+    @Override
     public void onConnectionFailed(ConnectionResult connectionResult) {
-        Log.i(TAG, "Connection failed. Error: " + connectionResult.getErrorCode());
+        Log.i(TAG, "Connection failed: " + connectionResult.getErrorCode());
     }
 }
